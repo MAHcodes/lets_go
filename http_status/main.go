66b20d2f6fa82a1331ae080e09a5995c57f9171e
mahcodes <mhmdali102@bk.ru>
2023-04-08 03:45:06 +0300

    add loading spinner

--- http_status/main.go
+++ http_status/main.go
@@ -6,19 +6,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 const url = "https://mah.codes"
 
 type model struct {
-	status int
-	err    error
+	spinner spinner.Model
+	status  int
+	err     error
 }
 
 type errMsg struct{ err error }
 
 type statusMsg int
+
+func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.MiniDot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	return model{spinner: s}
+}
 
 func checkServer() tea.Msg {
 	c := &http.Client{Timeout: 10 * time.Second}
@@ -36,7 +46,8 @@ func (e errMsg) Error() string {
 }
 
 func (m model) Init() tea.Cmd {
-	return checkServer
+  return m.spinner.Tick
+	// return tea.Batch(checkServer, m.spinner.Tick)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -52,30 +63,40 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		}
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
 	}
-  return m, nil
+	return m, nil
 }
 
 func (m model) View() string {
-  if m.err != nil {
-    return fmt.Sprintf("\nwell guess what? we have some serious problems: %s\n", m.err)
+	if m.err != nil {
+		return fmt.Sprintf("\nwell guess what? we have some serious problems: %s\n", m.err)
+	}
+
+  spinner := m.spinner.View() + " "
+	if m.status > 0 {
+    spinner = ""
   }
 
-  s := fmt.Sprintf("\nSending request to %s, please have a seat...\n", url)
 
-  if m.status > 0 {
-    s += fmt.Sprintf("%d: %s!\n", m.status, http.StatusText(m.status))
-  }
+	s := fmt.Sprintf("\n%sSending request to %s, please have a seat...\n", spinner, url)
 
-  s += "\nPress ctrl+c to quit.\n"
+	if m.status > 0 {
+		s += fmt.Sprintf("%d: %s!\n", m.status, http.StatusText(m.status))
+	}
 
-  return s
+	s += "\nPress ctrl+c to quit.\n"
+
+	return s
 }
 
-func main () {
-  _, err := tea.NewProgram(model{}).Run()
-  if err != nil {
-    fmt.Printf("Error: %v", err)
-    os.Exit(1)
-  }
+func main() {
+	_, err := tea.NewProgram(initialModel()).Run()
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
 }
